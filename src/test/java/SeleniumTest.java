e35ea2de2c29264e5db65fb054e3412daf62136d
cioloca535 <cioloca535@gmail.com>
2022-07-05 08:13:29 -0700

    Primul Proiect

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+
+        public static void main(String[] args) {
+            //System.setProperty("webdriver.chrome.driver", "src/test/chromedriver.exe");
+            //ChromeDriver driver = new ChromeDriver();
+
+
+        }
+
+    }
+
